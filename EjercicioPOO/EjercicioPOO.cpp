ba21a5669d8ae4eab93ad2a85c3cd3d6c24e031b
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-31 11:04:16 -0500

    ejercicio con tres constructores

--- EjercicioPOO/EjercicioPOO.cpp
+++ EjercicioPOO/EjercicioPOO.cpp
@@ -9,9 +9,12 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR cmdLine, int cmdSho
 
 void EjercicioPOO::Window_Open(Win::Event& e)
 {
-	NumerosComplejos a;
-	NumerosComplejos b(10.0);
-	NumerosComplejos c(5, 4);
+	NumerosComplejos x(10), y(2, 3), z[2];
+	z[0] = x;
+	z[1] = y;
+	wstring s = x.GetConjugado();
+	wstring s2 = y.GetConjugado();
+	z[0].imaginario = 18.2;
 	//x.real = 2.0;
 	//x.imaginario = 3.0;
 	//const double modulo = x.GetModulo();
@@ -20,11 +23,13 @@ void EjercicioPOO::Window_Open(Win::Event& e)
 	//Sys::Format(texto, L"%g,%g", modulo, angulo);
 	//this->MessageBox(texto, L"Numeros complejos", MB_OK);
 	this->tbxSalida.Text = L"x= ";
-	this->tbxSalida.Text += a.GetText();
+	this->tbxSalida.Text += x.GetText();
 	this->tbxSalida.Text += L"\r\nx= ";
-	this->tbxSalida.Text += b.GetText();
+	this->tbxSalida.Text += y.GetText();
 	this->tbxSalida.Text += L"\r\nx= ";
-	this->tbxSalida.Text += c.GetText();
+	this->tbxSalida.Text += z[0].GetText();
+	this->tbxSalida.Text += L"\r\nx= ";
+	this->tbxSalida.Text += z[1].GetText();
 	//wstring s = x.GetConjugado();
 	//this->tbxSalida.Text += x.GetText();
 	
